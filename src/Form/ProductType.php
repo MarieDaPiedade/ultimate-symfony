7e498af609d0cd05fa1bd1ac77c346f14ddea798
Marie DA PIEDADE <marie@dapiedade.org>
2022-03-08 14:58:25 +0100

    decouverte fonctionnalites avancees du composant form

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -4,8 +4,12 @@ namespace App\Form;
 
 use App\Entity\Product;
 use App\Entity\Category;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\FormBuilderInterface;
+use App\Form\DataTransformer\CentimesTransformer;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
@@ -35,6 +39,7 @@ class ProductType extends AbstractType
                 'attr' => [
                     'placeholder' => 'Tapez le prix du produit en euros'
                 ]
+                // pour faire la même chose qu'avec l'évènement en bcp plus rapide ; , 'divisor' => 100
             ])
             ->add('mainPicture', UrlType::class, [
                 'label' => 'Image du produit',
@@ -47,7 +52,43 @@ class ProductType extends AbstractType
                 'choice_label' => function (Category $category) { // ou 'name' qui indique que l'on veut récupérer le nom des catégories pour affichage
                     return strtoupper($category->getName()); // ici on créé une fonction pour faire un traitement sur les données (mettre en majuscule ici)
                 }
-            ]);;
+            ]);
+        // On utilise l'évènement crée dans la classe CentimesTransformer pour agir sur le prix
+        $builder->get('price')->addModelTransformer(new CentimesTransformer);
+
+        // Lorsque l'on veut rajouter un évènement
+
+        /*     $builder->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
+                $product = $event->getData();
+                if ($product->getPrice() !== null) {
+                    $product->setPrice($product->getPrice() * 100);
+                }
+           }); 
+
+                $builder->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event) {
+                $form = $event->getForm();
+
+                /**@var Product */
+
+        /*
+                $product = $event->getData();
+
+                if($product->getPrice() !== null) {
+                    $product->setPrice($product->getPrice() / 100);
+                }
+
+                 
+                if($product->getId() === null) {       //si le produit n'existe pas déjà, on rajoute la catégorie, sinon non
+                    $form->add('category', EntityType::class, [
+                        'label' => 'Category',
+                        'placeholder' => '-- Choisir une catégorie --',
+                        'class' => Category::class, // on explique que les données que l'on veut récupérer viennent de la classe category
+                        'choice_label' => function (Category $category) { // ou 'name' qui indique que l'on veut récupérer le nom des catégories pour affichage
+                            return strtoupper($category->getName()); // ici on créé une fonction pour faire un traitement sur les données (mettre en majuscule ici)
+                        }
+                    ]);
+                }
+            ); */
     }
 
     public function configureOptions(OptionsResolver $resolver): void
